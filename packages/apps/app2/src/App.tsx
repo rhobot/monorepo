88bbecd854e4ede2b2b4027d0bbb2ec021edbfeb
Ryan Rho <i@ryanorh.com>
2023-01-18 14:43:09 +0700

    Scaffold App1 and App2

---
+++ packages/apps/app2/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Hi! My name is App 2!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
